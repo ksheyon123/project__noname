19ee8575047db31f18a9feabf5060b19168445f1
Kangseohyun <kshyeon123@gmail.com>
2022-06-28 12:08:58 +0900

    Updatelayout updater

--- src/pages/LayoutPage.tsx
+++ src/pages/LayoutPage.tsx
@@ -1,35 +1,46 @@
 import React from "react";
 import styled from "styled-components";
-import { Button } from "src/components/index";
+import Store from "electron-store";
+import { Button, Input } from "src/components/index";
 
 const StyledLayoutPage = styled.div`
+  height : 100%;
   & > div.select-size {
     display: grid;
+    align-content: center;
     grid-template-rows: auto;
     grid-template-columns: 1fr 1fr 1fr;
     grid-gap: 10px;
-
-    @media screen and (max-width : 800px) {
-      display: grid;
-      grid-template-rows: auto;
-      grid-template-columns: 1fr 1fr;
-    }
-
+    width : 60%;
+    height : 100%;
+    padding : 10px;
     & > div.screen {
       display: flex;
       flex-direction: column;
+      text-align: center;
       &:before {
-        content : ",";
+        content : " ";
         height : 280px;
         border : 1px solid #AAA;
         border-radius : 5px;
+        @media screen and (max-width : 800px) {
+          height : 160px;
+        }
       }
     }
+    @media screen and (max-width : 800px) {
+      display: grid;
+      grid-template-rows: auto;
+      grid-template-columns: 1fr 1fr 1fr;
+
+    }
   }
-  
 `;
 
 const LayoutPage: React.FC = () => {
+
+  const store = new Store();
+
   return (
     <StyledLayoutPage>
       <div className="select-size">
@@ -42,6 +53,9 @@ const LayoutPage: React.FC = () => {
         <div className="screen">
           <span>2000 by 2000</span>
         </div>
+      </div>
+      <div className="input-size">
+
       </div>
     </StyledLayoutPage>
   )
