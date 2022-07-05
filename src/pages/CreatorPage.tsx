9650aaa1395a08f822e02eff47b621e5327a85b6
Kangseohyun <kshyeon123@gmail.com>
2022-07-05 20:46:51 +0900

    Fix draw grid once

--- src/pages/CreatorPage.tsx
+++ src/pages/CreatorPage.tsx
@@ -23,13 +23,13 @@ const StyledCreatorPage = styled.div`
       display: flex;
     }
   }
-
   & > div.wrap-canvas {
-    width : 1000px;
-    height : 1000px;
-    & > div {
-      overflow: hidden;
-    }
+    position : relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top : 30px;
+    overflow: hidden;
   }
   .pixel {
     border : 0.5px solid rgba(0, 0, 0, 0.1);
@@ -76,7 +76,7 @@ const CreatorPage: React.FC<IProps> = (props) => {
 
         const flexEl = document.createElement("div");
         flexEl.style.display = "flex";
-
+        flexEl.className = "row";
         for (let j = 0; j < width / size; j++) {
           const divEl = document.createElement("div");
           divEl.style.width = size + "px";
@@ -98,17 +98,19 @@ const CreatorPage: React.FC<IProps> = (props) => {
     }
   }
 
-  const handleEl = (e: MouseEventInit) => {
+  const handleEl = useCallback((e: MouseEventInit) => {
+    console.log(e.clientX);
+    console.log(ratio);
     if (e.clientX && e.clientY) {
       const blockCount = width / size;
       const coordX = Math.floor(e.clientX / size);
-      const coordY = Math.floor((e.clientY - 30) / size);
+      const coordY = Math.floor(((e.clientY - 30)) / size);
       const nth = coordX + (coordY * blockCount);
       const idx = nth;
       return idx;
     }
     return 0;
-  }
+  }, [ratio]);
 
   const handleOnMouseover = useCallback((e: any) => {
     if (!isDraw) return;
@@ -148,20 +150,22 @@ const CreatorPage: React.FC<IProps> = (props) => {
     }
   }, [isDraw]);
 
-  useEffect(() => {
-    const { current } = canvasCRef;
-    if (!!current) {
-      current.addEventListener("wheel", (e: WheelEvent) => {
-        if (e.deltaY > 0) {
-          setRatio(ratio - 0.02);
-        }
+  const handleOnWheel = useCallback((e: WheelEvent) => {
+    if (e.deltaY > 0) {
+      setRatio(ratio - 0.02);
+    }
 
-        if (e.deltaY < 0) {
-          setRatio(ratio + 0.02);
-        }
-      })
+    if (e.deltaY < 0) {
+      setRatio(ratio + 0.02);
     }
   }, [ratio]);
+
+  useEffect(() => {
+    const { current } = canvasCRef;
+    current?.addEventListener("wheel", handleOnWheel);
+    return () => current?.removeEventListener("wheel", handleOnWheel);
+  }, [ratio]);
+
   return (
     <StyledCreatorPage ref={divRef as RefObject<HTMLDivElement>}>
       <div className="wrap-tools">
@@ -172,6 +176,7 @@ const CreatorPage: React.FC<IProps> = (props) => {
         className="wrap-canvas"
       >
         <StyledCanvas
+          id="canvas"
           ref={canvasRef as RefObject<HTMLDivElement>}
           width={width}
           height={height}
